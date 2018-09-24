5116309174322beb3332a563f9ab7abd1e8a800c
Radek Bernatek <radek.bernatek@gmail.com>
2018-09-24 15:21:12 +0200

    Cleanup and updates

--- tests/Behat/Page/Shop/WelcomePageInterface.php
+++ tests/Behat/Page/Shop/WelcomePageInterface.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Tests\Acme\SyliusExamplePlugin\Behat\Page\Shop;
+namespace Tests\MangoSylius\MailChimpPlugin\Behat\Page\Shop;
 
 use Sylius\Behat\Page\PageInterface;
 
 interface WelcomePageInterface extends PageInterface
 {
-    /**
-     * @return string
-     */
-    public function getGreeting(): string;
+	/**
+	 * @return string
+	 */
+	public function getGreeting(): string;
 }
